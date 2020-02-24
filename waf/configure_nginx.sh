0bfc07b00861bfcecb6a68fe2b21e7118a09888e
lucian <lucian@m.bit-sentinel.com>
2020-02-24 21:50:19 +0200

    parametrised config for modsecurity

--- waf/configure_nginx.sh
+++ waf/configure_nginx.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
+export SEC_RULE_ENGINE=${SEC_RULE_ENGINE:-DetectionOnly}
 export HTTP_VERSION=${HTTP_VERSION:-1.1}
 export TARGET_SCHEME=${TARGET_SCHEME:-http}
 export TARGET_PORT=${TARGET_PORT:-80}
@@ -7,10 +8,10 @@ export TARGET_HOST_HEADER=${TARGET_HOST_HEADER:-${TARGET_HOST}}
 export CLIENT_MAX_BODY_SIZE=${CLIENT_MAX_BODY_SIZE:-20M}
 export SSL_PORT=${SSL_PORT:-443}
 
-REPLACEABLE='$HTTP_VERSION:$TARGET_SCHEME:$TARGET_PORT:$TARGET_HOST:$TARGET_HOST_HEADER:$CLIENT_MAX_BODY_SIZE:$SSL_PORT'
+REPLACEABLE='$SEC_RULE_ENGINE:$HTTP_VERSION:$TARGET_SCHEME:$TARGET_PORT:$TARGET_HOST:$TARGET_HOST_HEADER:$CLIENT_MAX_BODY_SIZE:$SSL_PORT'
 
 envsubst $REPLACEABLE < /nginx.conf.template > /usr/local/nginx/conf/nginx.conf
-
+envsubst $REPLACEABLE < /modsecurity.conf.template > /usr/local/nginx/conf/modsecurity.conf
 
 /add_self_signed_certs.sh
 
